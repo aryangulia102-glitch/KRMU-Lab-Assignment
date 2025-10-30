d507dfd466efcb596417c1bcf48740c36d653dda
aryangulia102-glitch <aryangulia102@gmail.com>
2025-10-30 15:44:02 +0530

    Add files via upload
    this is the abstraction method to calculate the area.....

---
+++ abstract.java
@@ -0,0 +1,55 @@
+public class Abstract {
+    class AbstractAreas {
+        public static void main(String args[]) {
+            Rectangle r = new Rectangle(9, 5);
+            Triangle t = new Triangle(10, 8);
+
+            Figure figref; // reference to Figure (abstract class)
+
+            figref = r;
+            System.out.println("Area is " + figref.area());
+
+            figref = t;
+            System.out.println("Area is " + figref.area());
+        }
+    }
+
+    // Using abstract methods and classes.
+    abstract class Figure {
+        double dim1;
+        double dim2;
+
+        Figure(double a, double b) {
+            dim1 = a;
+            dim2 = b;
+        }
+
+        // area is now an abstract method
+        abstract double area();
+    }
+
+    class Rectangle extends Figure {
+        Rectangle(double a, double b) {
+            super(a, b);
+        }
+
+        // override area for rectangle
+        double area() {
+            System.out.println("Inside Area for Rectangle.");
+            return dim1 * dim2;
+        }
+    }
+
+    class Triangle extends Figure {
+        Triangle(double a, double b) {
+            super(a, b);
+        }
+
+        // override area for right triangle
+        double area() {
+            System.out.println("Inside Area for Triangle.");
+            return dim1 * dim2 / 2;
+        }
+    }
+
+}
